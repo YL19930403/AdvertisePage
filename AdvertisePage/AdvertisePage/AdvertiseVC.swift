225fc39e9860a8d737ae7f180209cce96261d6c5
zhangli <313890735@qq.com>
2016-06-13 16:43:46 +0800

    导入SnapKit库

---
+++ AdvertisePage/AdvertisePage/AdvertiseVC.swift
@@ -0,0 +1,35 @@
+//
+//  AdvertiseVC.swift
+//  AdvertisePage
+//
+//  Created by 余亮 on 16/6/13.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+import UIKit
+
+class AdvertiseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AdvertisePage/AdvertisePage/AdvertiseView.swift
@@ -0,0 +1,59 @@
+//
+//  AdvertiseView.swift
+//  AdvertisePage
+//
+//  Created by 余亮 on 16/6/13.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+import UIKit
+
+class AdvertiseView: UIView {
+   
+
+    
+    //MARK: 懒加载
+    private lazy var adView : UIView = {
+        let view = UIView(frame:CGRectMake(0,0,100,100))
+        return
+    }()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
